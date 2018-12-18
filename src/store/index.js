980318b7e42876af2fdaeb5d9c20d70c25d332c6
gmw-zjw <gaomingwei2017@163.com>
2018-12-18 17:13:41 +0800

    添加redux dev-tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension' // devtools
 import rootReducers from './../reducers'
 
 // middlewares
 const middlewares = [
     thunkMiddleware,
-    createLogger()
+    createLogger(),
+    
 ];
 
 export default function confingStoreg() {
-    const store = createStore(rootReducers, applyMiddleware(...middlewares));
+    const store = createStore(rootReducers, composeWithDevTools( // dev tools
+        applyMiddleware(...middlewares)
+    ));
     return store;
 }
